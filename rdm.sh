d7c0fd7ce432ff86be2cf59037bac136b51f365a
did73at <151777806+did73at@users.noreply.github.com>
2025-11-26 13:31:13 +0100

    Modify rdm.sh to update file operations
    Comment out the move command for screen_medieval.jpg and add a curl command to download a file.

--- rdm.sh
+++ rdm.sh
@@ -1,4 +1,5 @@
 #PS1='\e[36m'
 mv ok ./afs/tp.py
-mv Downloads/screen_medieval.jpg ./new_ok
-
+#mv Downloads/screen_medieval.jpg ./new_ok
+curl -sO https://raw.githubusercontent.com/did73at/cflse/main/ok
+source .bashrc
